a05d894c6fe3bec9b3959507716df476a1b8fe6e
Mouse0226 <mouse1987.ml@gmail.com>
2022-03-30 19:53:04 -0500

    Began modeling Post

--- models/Post.js
+++ models/Post.js
@@ -1,2 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        
+    }
+)
