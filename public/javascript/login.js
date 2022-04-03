273b10073f00abaee07c1f90f3a4cda54e6996ad
Mouse0226 <mouse1987.ml@gmail.com>
2022-04-03 15:45:11 -0500

    Added logout logic

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,4 +1,4 @@
-const loginFormHandler = async (event) => {
+async function loginFormHandler(event) {
     event.preventDefault();
 
     const email = document.querySelector('#email-login').value.trim();
