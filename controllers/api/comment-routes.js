1004edd2661d050dcd8d7d0d798804c02f6252a6
Damon-Hansen <95259338+Damon-Hansen@users.noreply.github.com>
2022-04-06 09:34:06 -0500

    changed comment routes to async,await

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,24 +2,26 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', (req, res) => {
-  Comment.findAll()
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(error => {
-      res.status(500).json(error);
-    });
+router.get('/', async (req, res) => {
+    try {
+        const dbCommentData = await Comment.findAll()
+        res.json(dbCommentData)    
+    } catch (error) {
+          res.status(500).json(error);   
+    }
 });
 
-router.post('/', withAuth, (req, res) => {
-  Comment.create({
+router.post('/', withAuth, async (req, res) => {
+  try { 
+      const dbCommentCreateData = await Comment.create({
     comment_text: req.body.comment_text,
     user_id: req.session.user_id,
     post_id: req.body.post_id
   })
-    .then(dbCommentData => res.json(dbCommentData))
-    .catch(error => {
+  res.json(dbCommentCreateData)
+} catch (error) {
       res.status(400).json(error);
-    });
+}
 });
 
 module.exports = router;
