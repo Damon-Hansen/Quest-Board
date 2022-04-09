07e8403894e55fc518700ff33935214284d3b9e4
Mouse0226 <mouse1987.ml@gmail.com>
2022-04-08 19:54:30 -0500

    Fixed several handlebar issues

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,14 +1,14 @@
 async function loginFormHandler(event) {
     event.preventDefault();
 
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#validationServer01').value.trim();
+    const password = document.querySelector('#validationServerUsername').value.trim();
 
-    if (email && password) {
+    if (username && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                email,
+                username,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
